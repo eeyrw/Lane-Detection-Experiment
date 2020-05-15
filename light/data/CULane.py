6d1683c27baf94dd962e3f6f3a5a853caddc8a0a
yuan <yrw@sdec.com.cn>
2020-05-15 13:30:19 +0800

    M

--- light/data/CULane.py
+++ light/data/CULane.py
@@ -29,7 +29,7 @@ class CULaneDataset(Dataset):
     def __init__(self, rootDir,
                  split='train',
                  mode='discrete',
-                 framesGroupSize=13,
+                 framesGroupSize=2,
                  requireRawImage=False,
                  transformForSeg=None,
                  transformForImage=None,
