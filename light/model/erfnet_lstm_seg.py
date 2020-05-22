483fbce2f6f5bc8a2b41ed5cbb2848570951e99e
yuan <yrw@sdec.com.cn>
2020-05-22 15:40:31 +0800

    M

---
+++ light/model/erfnet_lstm_seg.py
@@ -0,0 +1,216 @@
+# ERFNet full model definition for Pytorch
+# Sept 2017
+# Eduardo Romera
+#######################
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+
+class CLSTM_cell(nn.Module):
+    """ConvLSTMCell
+    """
+    def __init__(self, shape, input_channels, filter_size, num_features):
+        super(CLSTM_cell, self).__init__()
+
+        self.shape = shape  # H, W
+        self.input_channels = input_channels
+        self.filter_size = filter_size
+        self.num_features = num_features
+        # in this way the output has the same size
+        self.padding = (filter_size - 1) // 2
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.input_channels + self.num_features,
+                      4 * self.num_features, self.filter_size, 1,
+                      self.padding),
+            nn.GroupNorm(4 * self.num_features // 32, 4 * self.num_features))
+
+    def forward(self, inputs=None, hidden_state=None, seq_len=10):
+        #  seq_len=10 for moving_mnist
+        if hidden_state is None:
+            hx = torch.zeros(inputs.size(1), self.num_features, self.shape[0],
+                             self.shape[1])
+            cx = torch.zeros(inputs.size(1), self.num_features, self.shape[0],
+                             self.shape[1])
+        else:
+            hx, cx = hidden_state
+        output_inner = []
+        for index in range(seq_len):
+            if inputs is None:
+                x = torch.zeros(hx.size(0), self.input_channels, self.shape[0],
+                                self.shape[1])
+            else:
+                x = inputs[index, ...]
+
+            combined = torch.cat((x, hx), 1)
+            gates = self.conv(combined)  # gates: S, num_features*4, H, W
+            # it should return 4 tensors: i,f,g,o
+            ingate, forgetgate, cellgate, outgate = torch.split(
+                gates, self.num_features, dim=1)
+            ingate = torch.sigmoid(ingate)
+            forgetgate = torch.sigmoid(forgetgate)
+            cellgate = torch.tanh(cellgate)
+            outgate = torch.sigmoid(outgate)
+
+            cy = (forgetgate * cx) + (ingate * cellgate)
+            hy = outgate * torch.tanh(cy)
+            output_inner.append(hy)
+            hx = hy
+            cx = cy
+        return torch.stack(output_inner), (hy, cy)
+
+class DownsamplerBlock (nn.Module):
+    def __init__(self, ninput, noutput):
+        super().__init__()
+
+        self.conv = nn.Conv2d(ninput, noutput-ninput, (3, 3), stride=2, padding=1, bias=True)
+        self.pool = nn.MaxPool2d(2, stride=2)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+
+    def forward(self, input):
+        output = torch.cat([self.conv(input), self.pool(input)], 1)
+        output = self.bn(output)
+        return F.relu(output)
+    
+
+class non_bottleneck_1d (nn.Module):
+    def __init__(self, chann, dropprob, dilated):        
+        super().__init__()
+
+        self.conv3x1_1 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1,0), bias=True)
+
+        self.conv1x3_1 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1), bias=True)
+
+        self.bn1 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.conv3x1_2 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1*dilated,0), bias=True, dilation = (dilated,1))
+
+        self.conv1x3_2 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1*dilated), bias=True, dilation = (1, dilated))
+
+        self.bn2 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.dropout = nn.Dropout2d(dropprob)
+        
+
+    def forward(self, input):
+
+        output = self.conv3x1_1(input)
+        output = F.relu(output)
+        output = self.conv1x3_1(output)
+        output = self.bn1(output)
+        output = F.relu(output)
+
+        output = self.conv3x1_2(output)
+        output = F.relu(output)
+        output = self.conv1x3_2(output)
+        output = self.bn2(output)
+
+        if (self.dropout.p != 0):
+            output = self.dropout(output)
+        
+        return F.relu(output+input)    #+input = identity (residual connection)
+
+
+class Encoder(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.initial_block = DownsamplerBlock(3,16)
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(DownsamplerBlock(16,64))
+
+        for x in range(0, 5):    #5 times
+           self.layers.append(non_bottleneck_1d(64, 0.03, 1)) 
+
+        self.layers.append(DownsamplerBlock(64,128))
+
+        for x in range(0, 2):    #2 times
+            self.layers.append(non_bottleneck_1d(128, 0.3, 2))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 4))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 8))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 16))
+
+        #Only in encoder mode:
+        self.output_conv = nn.Conv2d(128, num_classes, 1, stride=1, padding=0, bias=True)
+
+    def forward(self, input, predict=False):
+        output = self.initial_block(input)
+
+        for layer in self.layers:
+            output = layer(output)
+
+        if predict:
+            output = self.output_conv(output)
+
+        return output
+
+
+class UpsamplerBlock (nn.Module):
+    def __init__(self, ninput, noutput):
+        super().__init__()
+        self.conv = nn.ConvTranspose2d(ninput, noutput, 3, stride=2, padding=1, output_padding=1, bias=True)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+
+    def forward(self, input):
+        output = self.conv(input)
+        output = self.bn(output)
+        return F.relu(output)
+
+class Decoder (nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(UpsamplerBlock(128,64))
+        self.layers.append(non_bottleneck_1d(64, 0, 1))
+        self.layers.append(non_bottleneck_1d(64, 0, 1))
+
+        self.layers.append(UpsamplerBlock(64,16))
+        self.layers.append(non_bottleneck_1d(16, 0, 1))
+        self.layers.append(non_bottleneck_1d(16, 0, 1))
+
+        self.output_conv = nn.ConvTranspose2d( 16, num_classes, 2, stride=2, padding=0, output_padding=0, bias=True)
+
+    def forward(self, input):
+        output = input
+
+        for layer in self.layers:
+            output = layer(output)
+
+        output = self.output_conv(output)
+
+        return output
+
+#ERFNet
+class ERFNetLstm(nn.Module):
+    def __init__(self, num_classes, encoder=None):  #use encoder to pass pretrained encoder
+        super().__init__()
+
+        if (encoder == None):
+            self.encoder = Encoder(num_classes)
+        else:
+            self.encoder = encoder
+        self.decoder = Decoder(num_classes)
+
+    def forward(self, input, only_encode=False):
+        if only_encode:
+            return self.encoder.forward(input, predict=True)
+        else:
+            output = self.encoder(input)    #predict=False by default
+            return self.decoder.forward(output)
+
+def get_erfnet_lstm_seg(dataset='citys', pretrained=False, root='~/.torch/models',
+                               pretrained_base=False, **kwargs):
+    from light.data import datasets
+    model = ERFNetLstm(datasets[dataset].NUM_CLASS)
+    if pretrained:
+        from ..model import get_model_file
+        model.load_state_dict(
+            torch.load(get_model_file('erfnet_%s_best_model' % dataset, root=root), map_location='cpu'))
+    return model
+
+if __name__ == '__main__':
+    model = get_erfnet_lstm_seg()            
