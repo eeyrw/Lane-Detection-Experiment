efa1b3dc8ce71edbfb43812485ee6df659b69b3f
yuan <ayrw@163.com>
2020-05-28 19:18:53 +0800

    M

--- light/data/CULane.py
+++ light/data/CULane.py
@@ -185,7 +185,10 @@ class CULaneDataset(Dataset):
         for _, clips in rawframesGroupDict.items():
             clipNum = len(clips)
             for i in range(0, clipNum, self.framesGroupSize):
-                framesGroupList.append(clips[i:i + self.framesGroupSize])
+                if i + self.framesGroupSize <= len(clips):
+                    framesGroupList.append(clips[i:i + self.framesGroupSize])
+                else:
+                    framesGroupList.append(clips[len(clips)-self.framesGroupSize:]) # Ensure every group have same length
         return framesGroupList
 
     @property
